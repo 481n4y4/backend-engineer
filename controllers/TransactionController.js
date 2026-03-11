a38a935e11b49befb62832976490466fbe1e18de
481n4y4 <kheiraabinaya14@gmail.com>
2026-03-11 11:40:01 +0700

    ✨ feat(controller): create a controller

---
+++ controllers/TransactionController.js
@@ -0,0 +1,43 @@
+const Transaction = require("../models/Transaction");
+const midtransClient = require("midtrans-client");
+
+exports.createTransaction = async (req, res) => {
+  try {
+    const { first_name, amount, product_id } = req.body;
+
+    let snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.MIDTRANS_SERVERKEY,
+    });
+
+    const order_id = "ORDER-" + new Date().getTime();
+
+    const parameter = {
+      transaction_details: {
+        order_id: order_id,
+        gross_amount: amount,
+      },
+      credit_card: {
+        secure: true,
+      },
+      customer_details: {
+        first_name: first_name,
+      },
+    };
+
+    const transaction = await sanp.createTransaction(parameter);
+
+    const transactionUrl = transaction.redirect_url;
+
+    const newTransaction = new Transaction({
+      ...req.body,
+      midtrans_url: transactionUrl,
+      transaction_id: order_id,
+    });
+
+    await newTransaction.save();
+    res.status(201).json(newTransaction);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
